a11aa76407c7ad9dbfdd41870e2b4ee073f509b9
anoBand <bravepaul0802@gmail.com>
2025-10-28 19:26:22 +0900

    left setlist, all done

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 'public' 디렉터리의 정적 파일(html, css, js)을 제공합니다.
-app.use(express.static(path.join(__dirname, 'public')));
-
-// (게시판 기능 삭제)
-// /messages GET, POST 라우트(API)를 모두 제거했습니다.
+app.use(express.static('public'));
 
 // 서버 실행
 app.listen(PORT, () => {
